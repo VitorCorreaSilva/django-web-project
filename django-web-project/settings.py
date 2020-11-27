a71063c15bd939907d111d223bd05145ffb55727
VitorCorreaSilva <correa.vitor.silva@gmail.com>
2020-11-27 17:58:58 -0300

    add config postgres

--- django-web-project/settings.py
+++ django-web-project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django-web-project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
